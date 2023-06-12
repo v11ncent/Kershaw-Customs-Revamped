a1d7025e427ee1a64f799c832d0a6c74aaacc953
Vince <45806875+vince1444@users.noreply.github.com>
2023-06-12 03:52:23 -0400

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import critters from "astro-critters";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://kershawcustoms.com",
+  experimental: {
+    assets: true
+  },
+  integrations: [critters(), sitemap()]
+});
